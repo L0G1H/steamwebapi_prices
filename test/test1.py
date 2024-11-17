572165d0ebaf372d25add41f38f6960341f1455b
l0g1h <redfisher69@protonmail.com>
2024-11-17 19:17:24 +0200

    removed api key

--- test/test1.py
+++ test/test1.py
@@ -2,7 +2,7 @@ import steam_prices
 import os
 
 
-api_key = "BLCYCV7E0PLL3Q57" # os.getenv("steam_web_api"),
+api_key = os.getenv("steam_web_api")
 prices = steam_prices.get_prices(api_key, game="cs2", currency="EUR", return_type="dict")
 
 print(prices)
